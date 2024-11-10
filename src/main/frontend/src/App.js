5cb609af7a48fd5400d95540028de3d5a94d646e
sujinjeongg <102423490+sujinjeongg@users.noreply.github.com>
2024-11-10 22:41:10 +0900

    Add App.js

---
+++ src/main/frontend/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import CreateProject from "./components/create_project";
+import Result from "./components/result";
+
+function App() {
+   return (
+       <Router>
+           <Routes>
+               <Route path="/" element={<CreateProject />} />
+               <Route path="/result" element={<Result />} />
+           </Routes>
+       </Router>
+   );
+}
+
+export default App;
